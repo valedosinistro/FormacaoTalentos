698e24dbe316ef4c175f0976525033caed9308c3
Luís Lança <luis_lanca@outlook.com>
2018-08-05 23:11:25 -0300

    Máscaras
    CPF, CRM, Telefone, Data, CEP

--- Apresentacao/app/domain/paciente/pacienteCriar.js
+++ Apresentacao/app/domain/paciente/pacienteCriar.js
@@ -61,3 +61,69 @@ function inserirPaciente(paciente) {
             alert("Desculpe, ocorreu um erro no servidor.");
         });
 }
+
+// Mascáras 
+// function fMasc(objeto,mascara) {
+//     obj=objeto
+//     masc=mascara
+//     setTimeout("fMascEx()",1)
+// }
+// function fMascEx() {
+//     obj.value=masc(obj.value)
+// }
+// function mTel(tel) {
+//     tel=tel.replace(/\D/g,"")
+//     tel=tel.replace(/^(\d)/,"($1")
+//     tel=tel.replace(/(.{3})(\d)/,"$1)$2")
+//     if(tel.length == 9) {
+//         tel=tel.replace(/(.{1})$/,"-$1")
+//     } else if (tel.length == 10) {
+//         tel=tel.replace(/(.{2})$/,"-$1")
+//     } else if (tel.length == 11) {
+//         tel=tel.replace(/(.{3})$/,"-$1")
+//     } else if (tel.length == 12) {
+//         tel=tel.replace(/(.{4})$/,"-$1")
+//     } else if (tel.length > 12) {
+//         tel=tel.replace(/(.{4})$/,"-$1")
+//     }
+//     return tel;
+// }
+// function mCNPJ(cnpj){
+//     cnpj=cnpj.replace(/\D/g,"")
+//     cnpj=cnpj.replace(/^(\d{2})(\d)/,"$1.$2")
+//     cnpj=cnpj.replace(/^(\d{2})\.(\d{3})(\d)/,"$1.$2.$3")
+//     cnpj=cnpj.replace(/\.(\d{3})(\d)/,".$1/$2")
+//     cnpj=cnpj.replace(/(\d{4})(\d)/,"$1-$2")
+//     return cnpj
+// }
+// function mCPF(cpf){
+//     cpf=cpf.replace(/\D/g,"")
+//     cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
+//     cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
+//     cpf=cpf.replace(/(\d{3})(\d{1,2})$/,"$1-$2")
+//     return cpf
+// }
+// function mCEP(cep){
+//     cep=cep.replace(/\D/g,"")
+//     cep=cep.replace(/^(\d{2})(\d)/,"$1.$2")
+//     cep=cep.replace(/\.(\d{3})(\d)/,".$1-$2")
+//     return cep
+// }
+// function mNum(num){
+//     num=num.replace(/\D/g,"")
+//     return num
+// }
+
+$(document).ready(function(){
+    $('.date').mask('00/00/0000');
+    $('.time').mask('00:00:00');
+    $('.date_time').mask('00/00/0000 00:00:00');
+    $('.cep').mask('00000-000');
+    $('.phone').mask('0000-0000');
+    $('.phone_with_ddd').mask('(00) 0000-0000');
+    $('.phone_us').mask('(000) 000-0000');
+    $('.mixed').mask('AAA 000-S0S');
+    $('.cpf').mask('000.000.000-00', {reverse: true});
+    $('.crm').mask('000000/AA');
+    $('.money').mask('000.000.000.000.000,00', {reverse: true});
+  });
