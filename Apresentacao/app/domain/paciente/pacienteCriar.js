ec199143ce3f6288ed41dc08762d702c1284af42
Vanderlei Silva <vanderlei_nsilva@hotmail.com>
2018-08-05 23:22:56 -0300

    Paciente
    Alterado o campo celular e removido o campo crm

--- Apresentacao/app/domain/paciente/pacienteCriar.js
+++ Apresentacao/app/domain/paciente/pacienteCriar.js
@@ -120,11 +120,10 @@ $(document).ready(function(){
     $('.date_time').mask('00/00/0000 00:00:00');
     $('.cep').mask('00000-000');
     $('.phone').mask('0000-0000');
-    $('.phone_with_ddd').mask('(00) 0000-0000');
+    $('.phone_with_ddd').mask('(00) 00000-0000');
     $('.phone_us').mask('(000) 000-0000');
     $('.mixed').mask('AAA 000-S0S');
     $('.cpf').mask('000.000.000-00', {reverse: true});
-    $('.crm').mask('000000/AA');
     $('.money').mask('000.000.000.000.000,00', {reverse: true});
   });
 
