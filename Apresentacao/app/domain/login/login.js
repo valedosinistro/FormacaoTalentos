6b09c7826530190d9409fe78c3a04477a183912f
Luís Lança <luis_lanca@outlook.com>
2018-08-07 00:24:54 -0300

    Configurando API Paciente
    Paciente perfil e criar

--- Apresentacao/app/domain/login/login.js
+++ Apresentacao/app/domain/login/login.js
@@ -5,22 +5,13 @@ var elementosForm = {
     senha: document.querySelector('#senha')
 };
 
-document.querySelector('#login-link-cadastrar')
-    .addEventListener('click', function (event) {
-
-        event.preventDefault();
-
-        alert("Aguardando implementação.");
-
-    });
-
 document.querySelector('#form-login')
     .addEventListener('submit', function (event) {
 
         event.preventDefault();
 
         var obj = {
-            login: elementosForm.login.value,
+            user: elementosForm.login.value,
             senha: elementosForm.senha.value
         };
 
@@ -28,8 +19,52 @@ document.querySelector('#form-login')
 
     });
 
-function autenticarUsuario(obj){
-    console.log(obj);
+function autenticarUsuario(obj) {
 
-    window.location.href="../medico/medico.html";
+    var teste = $('input[name=login]:checked').val();
+    var apiatt;
+    if (teste === 'medico') {
+        // window.location.href = "../medico/medicoPerfil.html";
+        apiatt = api + 'medico';
+    } else {
+        // window.location.href = "../paciente/pacientePerfil.html";
+        apiatt = api + 'paciente';
+    }
+
+    var request = new Request(apiatt, {
+        method: "POST",
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        }),
+        body: JSON.stringify(obj)
+    });
+
+    fetch(request)
+        .then(function (response) {
+            console.log(response);
+            if (response.status == 200) {
+                response.json()
+                    .then(function (usuario) {
+                        if (teste === 'medico') {
+                            window.location.href = "../medico/medicoPerfil.html?id=" + usuario.id;
+                        } else {
+                            window.location.href = "../paciente/pacientePerfil.html?id=" + usuario.id;
+                        }
+                    });
+
+            } else {
+                response.json()
+                    .then(function (message) {
+                        alert(message.error);
+                    });
+            }
+        })
+        .catch(function (response) {
+            console.log(response);
+            alert("Desculpe, ocorreu um erro no servidor.");
+        });
 }
+
+$(document).ready(function () {
+    $('.senha').mask('AAAAAAAA');
+});
--- Apresentacao/app/domain/paciente/pacienteCriar.js
+++ Apresentacao/app/domain/paciente/pacienteCriar.js
@@ -62,58 +62,6 @@ function inserirPaciente(paciente) {
         });
 }
 
-// Mascáras 
-// function fMasc(objeto,mascara) {
-//     obj=objeto
-//     masc=mascara
-//     setTimeout("fMascEx()",1)
-// }
-// function fMascEx() {
-//     obj.value=masc(obj.value)
-// }
-// function mTel(tel) {
-//     tel=tel.replace(/\D/g,"")
-//     tel=tel.replace(/^(\d)/,"($1")
-//     tel=tel.replace(/(.{3})(\d)/,"$1)$2")
-//     if(tel.length == 9) {
-//         tel=tel.replace(/(.{1})$/,"-$1")
-//     } else if (tel.length == 10) {
-//         tel=tel.replace(/(.{2})$/,"-$1")
-//     } else if (tel.length == 11) {
-//         tel=tel.replace(/(.{3})$/,"-$1")
-//     } else if (tel.length == 12) {
-//         tel=tel.replace(/(.{4})$/,"-$1")
-//     } else if (tel.length > 12) {
-//         tel=tel.replace(/(.{4})$/,"-$1")
-//     }
-//     return tel;
-// }
-// function mCNPJ(cnpj){
-//     cnpj=cnpj.replace(/\D/g,"")
-//     cnpj=cnpj.replace(/^(\d{2})(\d)/,"$1.$2")
-//     cnpj=cnpj.replace(/^(\d{2})\.(\d{3})(\d)/,"$1.$2.$3")
-//     cnpj=cnpj.replace(/\.(\d{3})(\d)/,".$1/$2")
-//     cnpj=cnpj.replace(/(\d{4})(\d)/,"$1-$2")
-//     return cnpj
-// }
-// function mCPF(cpf){
-//     cpf=cpf.replace(/\D/g,"")
-//     cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
-//     cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
-//     cpf=cpf.replace(/(\d{3})(\d{1,2})$/,"$1-$2")
-//     return cpf
-// }
-// function mCEP(cep){
-//     cep=cep.replace(/\D/g,"")
-//     cep=cep.replace(/^(\d{2})(\d)/,"$1.$2")
-//     cep=cep.replace(/\.(\d{3})(\d)/,".$1-$2")
-//     return cep
-// }
-// function mNum(num){
-//     num=num.replace(/\D/g,"")
-//     return num
-// }
-
 $(document).ready(function(){
     $('.date').mask('00/00/0000');
     $('.time').mask('00:00:00');
@@ -124,6 +72,7 @@ $(document).ready(function(){
     $('.phone_us').mask('(000) 000-0000');
     $('.mixed').mask('AAA 000-S0S');
     $('.cpf').mask('000.000.000-00', {reverse: true});
+    $('.senha').mask('AAAAAAAA');
     $('.money').mask('000.000.000.000.000,00', {reverse: true});
   });
 
