9702aa2a9f2b38943bbdcd6e338bb369135d1c6e
Vanderlei Silva <vanderlei_nsilva@hotmail.com>
2018-08-08 03:48:47 -0300

    Variadas
    -Dash Paciente
    -Agendar consulta

--- Apresentacao/app/domain/dashboard/pacienteDash.js
+++ Apresentacao/app/domain/dashboard/pacienteDash.js
@@ -2,26 +2,5 @@ var urlParams = new URLSearchParams(location.search);
 var idPaciente = urlParams.get('id');
 
 var link = document.querySelector('.link');
-link.href = '../paciente/pacientePerfil.html?id=' + idPaciente; 
+link.href = '../paciente/pacientePerfil.html?id=' + idPaciente;
 
-jQuery(function ($) {
-
-    $(".sidebar-dropdown > a").click(function () {
-        $(".sidebar-submenu").slideUp(200);
-        if ($(this).parent().hasClass("active")) {
-            $(".sidebar-dropdown").removeClass("active");
-            $(this).parent().removeClass("active");
-        } else {
-            $(".sidebar-dropdown").removeClass("active");
-            $(this).next(".sidebar-submenu").slideDown(200);
-            $(this).parent().addClass("active");
-        }
-
-    });
-
-    $("#toggle-sidebar").click(function () {
-        $(".page-wrapper").toggleClass("toggled");
-    });
-   
-   
-});
