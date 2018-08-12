fbb9d4f536e20be9bc315d994efe32e7c01f2727
Vanderlei Silva <vanderlei_nsilva@hotmail.com>
2018-08-12 04:59:24 -0300

    Consulta
    Adicionado tela para agendar consultas

--- Apresentacao/app/domain/dashboard/pacienteDash.js
+++ Apresentacao/app/domain/dashboard/pacienteDash.js
@@ -3,3 +3,8 @@ var idPaciente = urlParams.get('id');
 
 var link = document.querySelector('.link');
 link.href = '../paciente/pacientePerfil.html?id=' + idPaciente; 
+
+
+
+var link2 = document.querySelector('#agendar');
+link2.href = '../dashboard/agendarConsulta.html?id=' + idPaciente; 
