ab2e7621561db117ea77c2eb8109a331d8291f1f
Pedro Bruini <pedro.bruini@hotmail.com>
2018-08-13 21:37:54 -0300

    Retorno da Senha

--- Fatec.Clinica.Dominio/Dto/PacienteDto.cs
+++ Fatec.Clinica.Dominio/Dto/PacienteDto.cs
@@ -9,6 +9,7 @@ namespace Fatec.Clinica.Dominio.Dto
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
+        public string Senha { get; set; }
         public string Cpf { get; set; }
         public char Sexo { get; set; }
         public string Telefone { get; set; }
